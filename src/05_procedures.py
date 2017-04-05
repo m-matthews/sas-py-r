60a89179bfd64082334f880f425bcac60f1defbd
Michael Matthews <michaelm@innotwist.com>
2017-04-05 16:32:01 +1000

    Add functions and start procedures in Python.

---
+++ src/05_procedures.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+# Import data produced with 'random.py'.
+dfpolicies = pd.read_csv("../data/policies.csv")
+dfclaims = pd.read_csv("../data/claims.csv")
+
+# Split data into under 25 and others using WHERE clauses.
+df_under25 = dfpolicies[dfpolicies['age']<25]
+df_25plus = dfpolicies[dfpolicies['age']>=25]
+
+# -----------
+# PROC APPEND
+# -----------
+
+# Method 1 - pd.concat accepts a list of dataframes.
+df_appended1 = pd.concat([df_under25, df_25plus])
+
+# Method 2 - pd.append adds a dataframe to an existing dataframe.
+df_appended2 = dfpolicies[dfpolicies['age']<25]
+df_appended2 = df_under25.append(df_25plus)
+
+# ---------
+# PROC FCMP
+# ---------
+
+# ---------
+# PROC FREQ
+# ---------
+
+# Count of rows by Make.
+df_freq1 = dfpolicies.groupby(['make'])['make'].count().to_frame()
+df_freq1['cumulative'] = df_freq1.cumsum()
+
+# Count of non-missing data by Make.
+df_freq2 = dfpolicies.groupby(['make'])['make', 'cancel_date'].count()
+
+# --------------------
+# PROC MEANS / SUMMARY
+# --------------------
+
+# Average Age by Make.
+df_summary1 = dfpolicies.groupby(['make'])['age'].mean().to_frame()
+
+# ---------
+# PROC SORT
+# ---------
+
+# --------------
+# PROC TRANSPOSE
+# --------------
+
+
