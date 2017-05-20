32f5304ccb4ad79afa4ed07255c2a4b93884f042
Michael Matthews <m-matthews@users.noreply.github.com>
2017-05-20 13:40:15 +1000

    Update Python URL comments.

--- src/11_internet.py
+++ src/11_internet.py
@@ -10,7 +10,9 @@ r = requests.get("https://api.github.com/users/m-matthews")
 
 print("\nEncoding:", r.encoding)
 
+# Read as a text stream.
 print("\nText:", r.text[:20], "...")
 
+# Read and convert JSON data.
 j = r.json()
 print("\nName:", j["name"], "\nLocation:", j["location"])
